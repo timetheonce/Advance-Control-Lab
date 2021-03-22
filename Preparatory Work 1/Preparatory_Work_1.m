c9b9c23226d0b871446e06f926482a00b057080d
timetheonce <49587703+timetheonce@users.noreply.github.com>
2021-03-22 17:21:15 +0200

    Update Preparatory_Work_1.m

--- Preparatory Work 1/Preparatory_Work_1.m
+++ Preparatory Work 1/Preparatory_Work_1.m
@@ -21,16 +21,26 @@ La=620e-6; %[H]
 J=0.06; %[kg m2]
 f=0.01; %[Nm sec / rad]
 
+% with La
 G3=1/(Ra+La*s);
 G4=Km;
 G5=1/(J*s+f);
 G6=Kb;
 Gvt=feedback(G3*G4*G5,G6)/s;
 
+% without La
 La=0;
 G3s=1/(Ra+La*s);
 G4s=Km;
 G5s=1/(J*s+f);
 G6s=Kb;
 Gvts=feedback(G3s*G4s*G5s,G6s)/s;
+
+% plot the bode with and without La
+figure
+bode(Gvt,'r')
+hold on
+bode(Gvts,'g')
+hold off
+legend('with La','without La')
 
