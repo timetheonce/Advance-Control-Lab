1556fad4f586276b617bb20cab671868aa58fb4a
CH3CH2OH66 <81168978+CH3CH2OH66@users.noreply.github.com>
2021-03-22 16:16:23 +0200

    Update Preparatory Work 1.m

--- Preparatory Work 1/Preparatory Work 1.m
+++ Preparatory Work 1/Preparatory Work 1.m
@@ -13,3 +13,14 @@ step(G2)
 grid on
 title('Step response for $G_2$','Interpreter','latex')
 
+%%
+Km=39.6/1000;
+Kb=6.46/1000/pi*60;
+Ra=6.8;
+La=620e-6;
+J=0.06;
+f=0.01;
+G3=1/(Ra+La*s);
+G4=Km;
+G5=1/(J*s^2+f*s);
+G6=Kb*s;
