3f91bb8e1ed23405eb11269ff4f33f90e42a2dd2
timetheonce <49587703+timetheonce@users.noreply.github.com>
2021-04-20 14:20:43 +0300

    Good

--- Final Report 1/jclab1.m
+++ Final Report 1/jclab1.m
@@ -1,15 +1,18 @@
 clear; clc; close all;
 load('data_1.mat')
 figure()
-plot(r_1.time,r_1.signals.values)
+plot(r_1.time,r_1.signals.values,'linewidth',2)
 xlabel('time')
 ylabel('amplitude')
+grid on
 figure()
-plot(u_1.time,u_1.signals.values)
+plot(u_1.time,u_1.signals.values,'linewidth',2)
 xlabel('time')
 ylabel('amplitude')
+grid on
 figure()
-plot(y_1.time,y_1.signals.values)
+plot(y_1.time,y_1.signals.values,'linewidth',2)
 xlabel('time')
 ylabel('arm degree[^o]')
+grid on
 ylim([-360 360])
